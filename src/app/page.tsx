50e0995183d765557419a275d8ef99d500574ce6
Fenerz07 <hugom2005@free.fr>
2025-01-16 00:44:09 +0100

    feat: Header first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "../components/header";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      <Header/>
+      <div>
+        <h1>Portfolio</h1>
+      </div>
+    </>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Header() {
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+    const handleMouseEnter = (index: number) => {
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
+    return (
+        <header className="py-10">
+            <nav className="flex justify-center space-x-10 list-none font-bold font-sans text-xl">
+                {['Acceuil', 'Bio', 'Projets', 'CV', 'Contact'].map((text, index) => (
+                    <li
+                        key={index}
+                        className={`transition duration-300 ${hoveredIndex != index ? hoveredIndex !== null ? 'brightness-50' : 'hover:brightness-50': ''}`}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <a href={`#${text.toLowerCase()}`}>{text}</a>
+                    </li>
+                ))}
+            </nav>
+        </header>
+    );
+}
