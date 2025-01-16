be65304fa3cde3e9309418e2f69eefb96e28e05e
Fenerz07 <hugom2005@free.fr>
2025-01-16 11:05:14 +0100

    feat: Footer and his SVGs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <div>
         <h1>Portfolio</h1>
       </div>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Footer() {
+    return (
+        <footer className='fixed bottom-5 w-full flex justify-center'>
+            <div className=' md:w-auto'>
+                <nav className='flex list-none space-x-4'>
+                    <li><a href='https://github.com/Fenerz07'><Image src="/assets/images/github.svg" alt="github" width={40} height={40}/></a></li>
+                    <li><a href='https://www.linkedin.com/in/hugo-matyla/'><Image src="/assets/images/linkedin.svg" alt="linkedin" width={40} height={40}/></a></li>
+                    <li><a href='mailto:hugom2005@free.fr'><Image src="/assets/images/mail.svg" alt="mail" width={40} height={40}/></a></li>
+                </nav>
+            </div>
+        </footer>
+    );
+}
