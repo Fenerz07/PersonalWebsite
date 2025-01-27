19a713ebddd9285d8e229a7029967239d62f9433
Fenerz07 <hugom2005@free.fr>
2025-01-27 12:35:10 +0100

    fix: Projects grid and Bio style

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -10,21 +10,30 @@ export default function Bio(){
                 <div className='border-2 border-white p-10'>
                     <div className="display flex justify-center items-center mb-5">
                         <Image src="/images/human.svg" alt="human" className="human" width={65} height={65} />
-                        <h1 className='text-3xl font-sans font-bold'>MOI, TOUT SIMPLEMENT</h1>
+                        <h1 className='text-3xl font-sans font-bold relative'>
+                            MOI, TOUT SIMPLEMENT
+                            <span className='absolute left-8 top-6 w-40 h-3 bg-gradient-to-r from-pink-500 to-yellow-500' style={{ zIndex: -1 }}></span>
+                        </h1>
                     </div>
                     <p>Je m&apos;appelle Hugo, jeune développeur de 19ans passioné par le sport, la rétromobile et évidemment les jeux vidéos ;)</p>
                 </div>
                 <div className='border-2 border-white p-10'>
                     <div className="display flex justify-center items-center mb-5">
                         <Image src="/images/book-mark.svg" alt="book-mark" className="book-mark" width={40} height={40} />
-                        <h1 className='text-3xl font-sans font-bold'>MON PARCOURS</h1>
+                        <h1 className='text-3xl font-sans font-bold relative'>
+                            MON PARCOURS
+                            <span className='absolute left-7 bottom-0 w-44 h-3 bg-gradient-to-r from-yellow-500 to-blue-500' style={{ zIndex: -1 }}></span>
+                        </h1>
                     </div>
                     <p>Je suis en 2ème année d&apos;études supérieur en Informatique, ça fait donc +4ans que je suis dans le dev grâce aux jeux vidéos </p>
                 </div>
                 <div className='border-2 border-white p-10'>
                     <div className="display flex justify-center items-center mb-5">
                         <Image src="/images/code.svg" alt="code" className="code" width={45} height={45} />
-                        <h1 className='text-3xl font-sans font-bold'>MON EXPERIENCE</h1>
+                        <h1 className='text-3xl font-sans font-bold relative'>
+                            MON EXPERIENCE
+                            <span className='absolute left-5 bottom-5 w-48 h-3 bg-gradient-to-r from-blue-500 to-purple-500' style={{ zIndex: -1 }}></span>
+                        </h1>
                     </div>
                     <p>Tout au long de mes études et de mon temps libre j&apos;ai pu découvrir certaints languages comme: Javascript, Python, C, React, SQL, C++, NodeJS, C# et d&apos;autres encore</p>
                 </div>
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,8 +6,9 @@ export default function Projects() {
           <div className='text-center relative mt-80 mb-10'>
               <h1 className='text-6xl font-bold font-sans'>MES PROJETS</h1>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
-            <a href="https://github.com/Fenerz07/BookManager" className="relative group overflow-hidden rounded-lg shadow-lg">
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-20">
+            <a href="https://github.com/Fenerz07/BookManager" className="relative group overflow-hidden  shadow-lg border-t-2 border-white">
                 <Image 
                 src="/images/book.svg" 
                 alt="Project 1" 
@@ -21,84 +22,84 @@ export default function Projects() {
             </div>
             </a>
   
-          <a href="https://github.com/Fenerz07/phpSocialNetwork-" className="relative group overflow-hidden rounded-lg shadow-lg">
-            <Image 
-              src="/images/social-network.svg"
-              alt="Project 2"
-              className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-              width={400}
-              height={400}
-            />
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">PHP - Réseau Social</h3>
-            </div>
-          </a>
-  
-          <a href="https://github.com/Fenerz07/MovieTracker" className="relative group overflow-hidden rounded-lg shadow-lg">
-            <Image 
-              src="/images/movie.svg" 
-              alt="Project 3" 
-              className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-              width={500}
-              height={300}
-            />
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">Nextjs - Traqueur de Films</h3>
-            </div>
-          </a>
-  
-          <a href="https://github.com/Fenerz07/Tournoi-Pokemon-Java" className="relative group col-span-2 overflow-hidden rounded-lg shadow-lg">
-            <Image 
-              src="/images/pokemon.svg" 
-              alt="Project 4" 
-              className="w-full h-96 scale-50 group-hover:scale-90 transition-all duration-300"
-              width={500}
-              height={300}
-            />
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">C# - Gestion de Livres</h3>
-            </div>
-          </a>
-  
-          <a href="https://github.com/Fenerz07/Puissance4-C" className="relative group overflow-hidden rounded-lg shadow-lg">
-            <Image 
-              src="/images/pin.svg" 
-              alt="Project 5" 
-              className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-              width={500}
-              height={300}
-            />
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">C - Puissance 4</h3>
-            </div>
-          </a>
-  
-          <a href="https://github.com/Fenerz07/TicTacToeJS" className="relative group overflow-hidden rounded-lg shadow-lg">
-            <Image 
-              src="/images/tic-tac-toe.svg" 
-              alt="Project 6" 
-              className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-              width={500}
-              height={300}
-            />
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">Javascript - Morpion</h3>
-            </div>
-          </a>
-  
-          <a href="https://github.com/Fenerz07/ENIGMA-Fast-Food-API" className="relative group col-span-2 overflow-hidden rounded-lg shadow-lg">
-            <Image 
-              src="/images/fast-food.svg" 
-              alt="Project 7" 
-              className="w-full h-96 scale-50 group-hover:scale-90 transition-all duration-300"
-              width={500}
-              height={300}
-            />
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">JS - API FastFood</h3>
-            </div>
-          </a>
-        </div>
-      </>
-    );
-  }
+            <a href="https://github.com/Fenerz07/phpSocialNetwork-" className="relative group overflow-hidden  shadow-lg border-t-2 border-white">
+              <Image 
+                src="/images/social-network.svg"
+                alt="Project 2"
+                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
+                width={400}
+                height={400}
+              />
+              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="text-white text-xl font-semibold">PHP - Réseau Social</h3>
+              </div>
+            </a>
+    
+            <a href="https://github.com/Fenerz07/MovieTracker" className="relative group overflow-hidden  shadow-lg border-t-2 border-r-2 border-white">
+              <Image 
+                src="/images/movie.svg" 
+                alt="Project 3" 
+                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
+                width={500}
+                height={300}
+              />
+              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="text-white text-xl font-semibold">Nextjs - Traqueur de Films</h3>
+              </div>
+            </a>
+    
+            <a href="https://github.com/Fenerz07/Tournoi-Pokemon-Java" className="relative group col-span-2 overflow-hidden shadow-lg border-t-2 border-l-2 border-white">
+              <Image 
+                src="/images/pokemon.svg" 
+                alt="Project 4" 
+                className="w-full h-48 scale-80 group-hover:scale-150 transition-all duration-300"
+                width={300}
+                height={300}
+              />
+              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
+                <h3 className="text-white text-xl font-semibold">C# - Gestion de Livres</h3>
+              </div>
+            </a>
+    
+            <a href="https://github.com/Fenerz07/Puissance4-C" className="relative group overflow-hidden  shadow-lg border-r-2 border-white">
+              <Image 
+                src="/images/pin.svg" 
+                alt="Project 5" 
+                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
+                width={500}
+                height={300}
+              />
+              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="text-white text-xl font-semibold">C - Puissance 4</h3>
+              </div>
+            </a>
+    
+            <a href="https://github.com/Fenerz07/TicTacToeJS" className="relative group overflow-hidden  shadow-lg border-l-2 border-b-2 border-white">
+              <Image 
+                src="/images/tic-tac-toe.svg" 
+                alt="Project 6"
+                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
+                width={500}
+                height={300}
+              />
+              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="text-white text-xl font-semibold">Javascript - Morpion</h3>
+              </div>
+            </a>
+    
+            <a href="https://github.com/Fenerz07/ENIGMA-Fast-Food-API" className="relative group col-span-2 overflow-hidden shadow-lg border-b-2 border-t-2 border-r-2 border-white">
+              <Image 
+                src="/images/fast-food.svg" 
+                alt="Project 7" 
+                className="w-full h-48 scale-80 group-hover:scale-150 transition-all duration-300"
+                width={300}
+                height={300}
+              />
+              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="text-white text-xl font-semibold">JS - API FastFood</h3>
+              </div>
+            </a>
+          </div>
+        </>
+  );
+}
