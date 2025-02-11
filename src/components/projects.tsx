89b697895fd63106edc410b12a5bf860ff022ead
Fenerz07 <hugom2005@free.fr>
2025-02-11 23:20:54 +0100

    fix: projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,105 +1,93 @@
 import Image from 'next/image';
 
 export default function Projects() {
-    return (
-        <>
-          <div className='text-center relative mt-80 mb-10'>
-              <h1 className='text-6xl font-bold font-sans'>MES PROJETS</h1>
-          </div>
+  return (
+    <>
+      <div className='text-center mt-20 mb-10'>
+        <h1 className='text-4xl font-bold'>MES PROJETS</h1>
+      </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-20">
-            <a href="https://github.com/Fenerz07/BookManager" className="relative group overflow-hidden  shadow-lg border-t-2 border-white">
-                <Image 
-                src="/images/book.svg" 
-                alt="Project 1" 
-                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-                width={400}
-                height={200}
-                />
-            
-            <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="text-white text-xl font-semibold">Java - Tournoi Pokemon</h3>
-            </div>
-            </a>
-  
-            <a href="https://github.com/Fenerz07/phpSocialNetwork-" className="relative group overflow-hidden  shadow-lg border-t-2 border-white">
-              <Image 
-                src="/images/social-network.svg"
-                alt="Project 2"
-                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-                width={400}
-                height={400}
-              />
-              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h3 className="text-white text-xl font-semibold">PHP - Réseau Social</h3>
-              </div>
-            </a>
-    
-            <a href="https://github.com/Fenerz07/MovieTracker" className="relative group overflow-hidden  shadow-lg border-t-2 border-r-2 border-white">
-              <Image 
-                src="/images/movie.svg" 
-                alt="Project 3" 
-                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-                width={500}
-                height={300}
-              />
-              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h3 className="text-white text-xl font-semibold">Nextjs - Traqueur de Films</h3>
-              </div>
-            </a>
-    
-            <a href="https://github.com/Fenerz07/Tournoi-Pokemon-Java" className="relative group col-span-2 overflow-hidden shadow-lg border-t-2 border-l-2 border-white">
-              <Image 
-                src="/images/pokemon.svg" 
-                alt="Project 4" 
-                className="w-full h-48 scale-80 group-hover:scale-150 transition-all duration-300"
-                width={300}
-                height={300}
-              />
-              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
-                <h3 className="text-white text-xl font-semibold">C# - Gestion de Livres</h3>
-              </div>
-            </a>
-    
-            <a href="https://github.com/Fenerz07/Puissance4-C" className="relative group overflow-hidden  shadow-lg border-r-2 border-white">
-              <Image 
-                src="/images/pin.svg" 
-                alt="Project 5" 
-                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-                width={500}
-                height={300}
-              />
-              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h3 className="text-white text-xl font-semibold">C - Puissance 4</h3>
-              </div>
-            </a>
-    
-            <a href="https://github.com/Fenerz07/TicTacToeJS" className="relative group overflow-hidden  shadow-lg border-l-2 border-b-2 border-white">
-              <Image 
-                src="/images/tic-tac-toe.svg" 
-                alt="Project 6"
-                className="w-full h-48 scale-50 group-hover:scale-90 transition-all duration-300"
-                width={500}
-                height={300}
-              />
-              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h3 className="text-white text-xl font-semibold">Javascript - Morpion</h3>
-              </div>
-            </a>
-    
-            <a href="https://github.com/Fenerz07/ENIGMA-Fast-Food-API" className="relative group col-span-2 overflow-hidden shadow-lg border-b-2 border-t-2 border-r-2 border-white">
-              <Image 
-                src="/images/fast-food.svg" 
-                alt="Project 7" 
-                className="w-full h-48 scale-80 group-hover:scale-150 transition-all duration-300"
-                width={300}
-                height={300}
-              />
-              <div className="absolute inset-0 bg-black  flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h3 className="text-white text-xl font-semibold">JS - API FastFood</h3>
-              </div>
-            </a>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
+        
+        <a href="https://github.com/Fenerz07/Tournoi-Pokemon-Java" target="_blank" rel="noopener noreferrer" className="relative group overflow-hidden shadow-lg border border-gray-200 rounded-lg">
+          <Image 
+            src="/images/pokemon.svg" 
+            alt="Tournoi Pokemon en Java" 
+            className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={250}
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <h3 className="text-white text-xl font-semibold">Java - Tournoi Pokemon</h3>
           </div>
-        </>
+        </a>
+
+        <a href="https://github.com/Fenerz07/phpSocialNetwork-" target="_blank" rel="noopener noreferrer" className="relative group overflow-hidden shadow-lg border border-gray-200 rounded-lg">
+          <Image 
+            src="/images/social-network.svg"
+            alt="Réseau Social en PHP"
+            className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={250}
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <h3 className="text-white text-xl font-semibold">PHP - Réseau Social</h3>
+          </div>
+        </a>
+
+        <a href="https://github.com/Fenerz07/MovieTracker" target="_blank" rel="noopener noreferrer" className="relative group overflow-hidden shadow-lg border border-gray-200 rounded-lg">
+          <Image 
+            src="/images/movie.svg" 
+            alt="Traqueur de Films en Next.js" 
+            className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={250}
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <h3 className="text-white text-xl font-semibold">Next.js - Traqueur de Films</h3>
+          </div>
+        </a>
+
+        <a href="https://github.com/Fenerz07/BookManager" target="_blank" rel="noopener noreferrer" className="relative group overflow-hidden shadow-lg border border-gray-200 rounded-lg">
+          <Image 
+            src="/images/book.svg" 
+            alt="Gestion de Livres en C#" 
+            className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={250}
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <h3 className="text-white text-xl font-semibold">C# - Gestion de Livres</h3>
+          </div>
+        </a>
+
+        <a href="https://github.com/Fenerz07/Puissance4-C" target="_blank" rel="noopener noreferrer" className="relative group overflow-hidden shadow-lg border border-gray-200 rounded-lg">
+          <Image 
+            src="/images/pin.svg" 
+            alt="Puissance 4 en C" 
+            className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={250}
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <h3 className="text-white text-xl font-semibold">C - Puissance 4</h3>
+          </div>
+        </a>
+
+        <a href="https://github.com/Fenerz07/TicTacToeJS" target="_blank" rel="noopener noreferrer" className="relative group overflow-hidden shadow-lg border border-gray-200 rounded-lg">
+          <Image 
+            src="/images/tic-tac-toe.svg" 
+            alt="Morpion en JavaScript"
+            className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={250}
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <h3 className="text-white text-xl font-semibold">JavaScript - Morpion</h3>
+          </div>
+        </a>
+
+      </div>
+    </>
   );
 }
